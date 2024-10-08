bf85501cdd078fd69dbd4d03967f6467e6838e56
José Luiz Borin Junior <jose.borin@r2u.io>
2024-10-07 21:25:37 -0300

    Rename register

--- features/step_definitions/cadastro_produtos_steps.rb
+++ features/step_definitions/cadastro_produtos_steps.rb
@@ -1,28 +1,28 @@
-Dado('que estou na página de cadastro de produto') do
+Given('que estou na página de cadastro de produto') do
   visit '/products/new'
 end
 
-Quando('preencho o campo nome {string} com {string}') do |field, value|
+When('preencho o campo nome {string} com {string}') do |field, value|
   fill_in field, with: value
 end
 
-Quando('preencho o campo preço {string} com {string}') do |field, value|
+When('preencho o campo preço {string} com {string}') do |field, value|
   fill_in field, with: value
 end
 
-Quando('preencho o campo categoria {string} com {string}') do |field, value|
+When('preencho o campo categoria {string} com {string}') do |field, value|
   fill_in field, with: value
 end
 
-Quando('seleciono o campo {string}') do |field|
+When('seleciono o campo {string}') do |field|
   check field
 end
 
-Quando('clico no botão {string}') do |button|
+When('clico no botão {string}') do |button|
   click_button button
 end
 
-Então('devo ter cadastrado o produto \({string}, {float}, {string}, {string})') do |name, price, category, require_ingredients|
+Then('devo ter cadastrado o produto \({string}, {float}, {string}, {string})') do |name, price, category, require_ingredients|
   product = Product.find_by(name: name)
 
   expect(product).to_not be_nil
@@ -31,7 +31,7 @@ Então('devo ter cadastrado o produto \({string}, {float}, {string}, {string})')
   expect(product.require_ingredients).to eq(require_ingredients == "true")
 end
 
-Quando('preencho o formulário \({string}, {string}, {string}, {string}) com dados inválidos \({string}, {string}, {string}, {string})') do |name_field, price_field, category_field, require_ingredients_checkbox, name_value, price_value, category_value, require_ingredients_checked|
+When('preencho o formulário \({string}, {string}, {string}, {string}) com dados inválidos \({string}, {string}, {string}, {string})') do |name_field, price_field, category_field, require_ingredients_checkbox, name_value, price_value, category_value, require_ingredients_checked|
   fill_in name_field, with: name_value
   fill_in price_field, with: price_value
   fill_in category_field, with: category_value
@@ -43,7 +43,7 @@ Quando('preencho o formulário \({string}, {string}, {string}, {string}) com dad
   end
 end
 
-Então('devo ver erros de cadastro do produto') do
+Then('devo ver erros de cadastro do produto') do
   expect(page.status_code).to eq(422)
 
   expect(page).to have_content("Name can't be empty")
@@ -52,7 +52,7 @@ Então('devo ver erros de cadastro do produto') do
   expect(page).to have_content("Category can't be empty")
 end
 
-Então('devo ver o erro de preço do produto') do
+Then('devo ver o erro de preço do produto') do
   expect(page.status_code).to eq(422)
 
   expect(page).to have_content("Price must be greater than 0")
