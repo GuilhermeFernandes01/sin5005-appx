88ce6365dd7284239438cf904f10470c65b48c70
José Luiz Borin Junior <jose.borin@r2u.io>
2024-10-08 11:06:03 -0300

    Bug fix.

--- features/step_definitions/deletar_produto_steps.rb
+++ features/step_definitions/deletar_produto_steps.rb
@@ -5,10 +5,12 @@ Given('estou na página do produto {string}') do |string|
 end
 
 When('clico no link para deletar o produto') do
-  click_link('Deletar')
+  click_button('Deletar')
 end
 
 Then('devo ver o produto {string} e não o produto {string}') do |product1, product2|
+  expect(page).to have_current_path('/products', ignore_query: true)
+
   expect(page).to have_content(product1)
   expect(page).to have_no_content(product2)
 end
