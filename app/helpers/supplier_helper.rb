44b281cc1b9570a73770ba16568a022ac81f772f
Carolina Thomaz <lorac.thomaz@gmail.com>
2024-10-14 15:54:46 -0300

    verificacao do rubocop

--- app/helpers/supplier_helper.rb
+++ app/helpers/supplier_helper.rb
@@ -8,6 +8,6 @@ module SupplierHelper
   end
 
   def format_phone(phone)
-    phone.gsub(/\D/, '').gsub(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3')
+    phone.gsub(/\D/, "").gsub(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3")
   end
 end
