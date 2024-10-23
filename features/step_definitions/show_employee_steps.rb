f97488bba67812e19f1aa635dcaa0e1d0e84862a
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2024-10-22 22:27:58 -0300

    fix: tests for show employee feature

---
+++ features/step_definitions/show_employee_steps.rb
@@ -0,0 +1,7 @@
+When('I click the button to show details of the employee {string}') do |employee_name|
+  click_link "Exibir funcionário", match: :first
+end
+
+Then('I should see the employee details containing {string}') do |text|
+  expect(page).to have_content(text)
+end
