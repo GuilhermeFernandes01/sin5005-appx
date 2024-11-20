92b22d8aa768a27d58dafeec21ff0aa9d59f09e0
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2024-11-20 19:34:58 -0300

    fix: fixes the warning Rails 7.1 has deprecated the singular fixture_path in favour of an array

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,9 +83,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include FactoryBot::Syntax::Methods
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
